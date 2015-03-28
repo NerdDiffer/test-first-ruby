2a026d11c04ad1bd19e6c1748b434b678be19050
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-03-27 20:17:40 -0700

    dictionary class & tests

--- lib/11_dictionary.rb
+++ lib/11_dictionary.rb
@@ -2,13 +2,35 @@ class Dictionary
   attr_reader :entries
 
   def initialize
-    @entries ||= {}
+    @entries = {}
   end
 
   def add(entry)
-    return ArgumentError unless entry.class == Hash
-    print entry
-    puts
-    @entries[entry[:name]] = entry[:value]
+    if entry.class == Hash
+      key = entry.keys[0]
+      val = entry.values[0]
+      @entries[key] = val
+    end
+    @entries[entry] = nil if entry.class == String
   end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(x)
+    keywords.include?(x)
+  end
+
+  def find(x)
+    @entries.select { |e| e.include?(x) }
+  end
+
+  def printable
+    keywords.map do |key|
+      val = @entries[key]
+      "[#{key}] \"#{val}\""
+    end.join("\n")
+  end
+
 end
