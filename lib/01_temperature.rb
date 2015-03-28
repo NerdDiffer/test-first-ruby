f01ab3601f772d392c0fb798e53ba4bb8d1483c1
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-03-27 19:08:00 -0700

    apply temperature files from stash

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,47 +1,14 @@
-class Temperature
-  def initialize options = {}
-    options.each do |name, value|
-      instance_variable_set "@#{name}", value
-    end
-  end
+ratio = (9.0 / 5.0)
+diff = 32.0
 
-  def in_fahrenheit
-    if self.instance_variable_defined?(:@c)
-      Temperature.ctof(@c)
-    elsif self.instance_variable_defined?(:@f)
-      @f
-    end
-  end
-
-  def in_celsius
-    if self.instance_variable_defined?(:@f)
-      Temperature.ftoc(@f) 
-    elsif self.instance_variable_defined?(:@c)
-      @c
-    end
-  end
-
-  #class << self
-  #  alias :from_celsius :in_fahrenheit
-  #end
-
-  def self.ftoc degrees_fahrenheit
-    (degrees_fahrenheit - 32.0) * (5.0 / 9.0)
-  end
-  
-  def self.ctof degrees_celsius
-    (degrees_celsius * (9.0 / 5.0)) + 32.0
-  end
+define_method :ftoc do |degrees|
+  degrees -= diff
+  degrees /= ratio
+  degrees.round
 end
 
-class Celsius < Temperature
-  def initialize degrees
-    @c = degrees
-  end
-end
-
-class Fahrenheit < Temperature
-  def initialize degrees
-    @f = degrees
-  end
+define_method :ctof do |degrees|
+  degrees *= ratio
+  degrees += diff
+  degrees
 end
